56f7cff7a52a1ee573dc079eff3b933f5feb66ef
suyash chahande <suyashchahande59@gmail.com>
2023-03-22 16:41:23 +0530

    Second Commit

--- src/main/java/com/example/test/HelloWorldProjectApplication.java
+++ src/main/java/com/example/test/HelloWorldProjectApplication.java
@@ -8,6 +8,7 @@ public class HelloWorldProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldProjectApplication.class, args);
+		System.out.println("Hello World ");
 	}
 
 }
